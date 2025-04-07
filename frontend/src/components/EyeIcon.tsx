8550426d4f89d0f0aded80448a3e7a08d34727c7
Ayesha Mirza <56220176+ayesha-m14@users.noreply.github.com>
2025-04-08 02:48:13 +0500

    Create EyeIcon.tsx

---
+++ frontend/src/components/EyeIcon.tsx
@@ -0,0 +1,23 @@
+// src/components/SignUp/EyeIcon.tsx
+import React from "react"
+
+const EyeIcon: React.FC = () => (
+    <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="14"
+    height="14"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+    <circle cx="12" cy="12" r="3"></circle>
+  </svg>
+
+)
+
+export default EyeIcon
+
